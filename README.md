e625a36c94ac726838ae2d837c3107feb498b79d
Fries_I23 <FriesI23@outlook.com>
2025-06-20 18:33:41 +0800

    chore: add new repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Custom Homebrew cask repo for Table Habit
+
+## Usage
+
+```shell
+# add repo
+brew tap FriesI23/homebrew-mhabit
+# install
+brew install FriesI23/homebrew-mhabit/table-habit
+```
---
+++ table-habit.rb
@@ -0,0 +1,32 @@
+cask "table-habit" do
+  version "1.16.17+86"
+  sha256 "0e573df86692f716c93258f9da2bf195923218a60a807103a2781bd19c7505e5"
+
+  url "https://github.com/FriesI23/mhabit/releases/download/v#{version}/mhabit.dmg",
+      verified: "github.com/FriesI23/mhabit/"
+
+  name "Table Habit"
+  desc "A simple habit tracker"
+  homepage "https://github.com/FriesI23/mhabit"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
+
+  app "mhabit.app"
+
+  preflight do
+    system_command "xattr",
+                   args: ["-d", "com.apple.quarantine", "#{staged_path}/mhabit.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/io.github.friesi23.mhabit",
+    "~/Library/Caches/io.github.friesi23.mhabit",
+    "~/Library/Preferences/io.github.friesi23.mhabit.plist"
+  ]
+end
