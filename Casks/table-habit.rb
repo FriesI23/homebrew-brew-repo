494605da80cd8da9f0ad58f28b396249e06e0003
Fries_I23 <FriesI23@outlook.com>
2025-07-03 15:48:50 +0800

    feat: update repo

--- Casks/table-habit.rb
+++ Casks/table-habit.rb
@@ -2,19 +2,19 @@ cask "table-habit" do
   version "1.16.17+86"
   sha256 "0e573df86692f716c93258f9da2bf195923218a60a807103a2781bd19c7505e5"
 
-  url "https://github.com/FriesI23/mhabit/releases/download/v#{version}/mhabit.dmg",
-      verified: "github.com/FriesI23/mhabit/"
-
+  url "https://github.com/FriesI23/mhabit/releases/download/v#{version}/mhabit.dmg"
   name "Table Habit"
-  desc "A simple habit tracker"
+  desc "Simple habit tracker"
   homepage "https://github.com/FriesI23/mhabit"
 
   livecheck do
     url :url
-    strategy :github_latest
+    strategy :github_latest do |json|
+      json["tag_name"].delete_prefix("v")
+    end
   end
 
-  auto_updates true
+  auto_updates false
   depends_on macos: ">= :mojave"
 
   app "mhabit.app"
@@ -27,6 +27,6 @@ cask "table-habit" do
   zap trash: [
     "~/Library/Application Support/io.github.friesi23.mhabit",
     "~/Library/Caches/io.github.friesi23.mhabit",
-    "~/Library/Preferences/io.github.friesi23.mhabit.plist"
+    "~/Library/Preferences/io.github.friesi23.mhabit.plist",
   ]
 end
