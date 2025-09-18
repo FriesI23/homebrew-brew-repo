5db914f204417988c6085d0237e5efe45ed3a6f3
Fries_I23 <FriesI23@outlook.com>
2025-09-18 08:27:35 +0000

    table-habit 1.17.8+100

--- Casks/table-habit.rb
+++ Casks/table-habit.rb
@@ -1,6 +1,6 @@
 cask "table-habit" do
-  version "1.17.5+98"
-  sha256 "ca84a04538c8d450b06d01c9ce1d20b05e1900f65d485290c90ff6e14fa99312"
+  version "1.17.8+100"
+  sha256 "8a6ae858b3a9c3fc7eb13d4d9ab137c62fbc990e4d47c8cbc8f218d7eed4a259"
 
   url "https://github.com/FriesI23/mhabit/releases/download/v#{version}/mhabit.dmg"
   name "Table Habit"
