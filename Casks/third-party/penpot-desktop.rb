935448c9130a4da94896912df25d2c245fd53192
Fries_I23 <friesi23@disroot.org>
2026-08-27 22:19:55 +0000

    penpot-desktop 0.24.0

--- Casks/third-party/penpot-desktop.rb
+++ Casks/third-party/penpot-desktop.rb
@@ -1,14 +1,14 @@
 cask "penpot-desktop" do
-  version "0.23.2"
+  version "0.24.0"
 
   on_arm do
-    sha256 "f0dc9ca47660b0b03270413dde10cb1fc3329700e1a52f045db7017bc396fa4c"
+    sha256 "e8d4fb4af6ed33e2a0c280d8882b23af21b9c28d9c0cdba5206aa16e036b5496"
 
     url "https://github.com/author-more/penpot-desktop/releases/download/v#{version}/penpot-desktop-arm64.dmg",
         verified: "github.com/author-more/penpot-desktop/"
   end
   on_intel do
-    sha256 "03e70854a2902488963ca6a457c871819911402ced57bc48e35cf4ec2f4ea162"
+    sha256 "a5ecbebee355580eda6f176b26a8056487918f7fdfc4ec3ad26f1d4e205ee1d1"
 
     url "https://github.com/author-more/penpot-desktop/releases/download/v#{version}/penpot-desktop-x64.dmg",
         verified: "github.com/author-more/penpot-desktop/"
